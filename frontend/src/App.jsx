f1eef1965621dded10d2f26872adb030d93e1bfb
unknown <codedev1028@gmail.com>
2025-07-03 12:35:53 -0700

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           }
         />
         <Route path="/templates" element={<ProtectedRoute><Templates /></ProtectedRoute>} />
-      </Routes>
+      </Routes> 
     </BrowserRouter>
   );
 }
